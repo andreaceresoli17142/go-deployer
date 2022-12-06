ef145c3526653f2391874078f2f77dc6a699a558
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2022-12-06 15:47:12 +0100

    automated commit 06/12/2022 15:47

--- main.go
+++ main.go
@@ -3,36 +3,101 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
-	"github.com/go-git/go-git/v5"
-	_ "github.com/go-git/go-git/v5/_examples"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
+const (
+	defaultPolling = 10 // 10 sec polling
+)
+
+type JobType int
+
+const (
+	keepUpdated JobType = iota
+	keepPushing JobType = iota
+)
+
+type Repository struct {
+	Job     JobType `json:"job"`
+	Name    string  `json:"name"`
+	Url     string  `json:"url"`
+	Remote  string  `json:"remote"`
+	Path    string  `json:"path"`
+	Polling int     `json:"polling"`
+}
+
+func notify(s string) {
+	exec.Command("notify-send", "started").Run()
+}
+
 func main() {
+
 	path := os.Args[1]
+	remoteName := os.Args[2]
+
+	var publicKeys *ssh.PublicKeys
+
+	if len(os.Args) >= 4 {
+		privateKeyFile := os.Args[3]
+		var password string
+
+		if len(os.Args) == 5 {
+			password = os.Args[4]
+		}
+
+		_, err := os.Stat(privateKeyFile)
+		if err != nil {
+			fmt.Println("read file %s failed %s\n", privateKeyFile, err.Error())
+			return
+		}
+
+		// Clone the given repository to the given directory
+		publicKeys, err = ssh.NewPublicKeysFromFile("git", privateKeyFile, password)
+		if err != nil {
+			fmt.Println("generate publickeys failed: %s\n", err.Error())
+			return
+		}
+		fmt.Println(publicKeys)
+	}
+
+	if err := updateIfChanged(publicKeys, path, remoteName, true); err != nil {
+		fmt.Println(err)
+		notify(err.Error())
+	}
+}
+
+func updateIfChanged(sshAuth *ssh.PublicKeys, path string, remoteName string, force bool) (err error) {
 
 	local, err := git.PlainOpen(path)
 
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
 
 	localHead, err := local.Head()
 
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
 
-	remote, err := local.Remote("origin")
+	remote, err := local.Remote(remoteName)
 
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
 
-	references, err := remote.List(&git.ListOptions{})
+	listAuth := &git.ListOptions{}
+	if sshAuth != nil {
+		listAuth.Auth = sshAuth
+	}
+
+	references, err := remote.List(listAuth)
 
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
 
 	found, behind := false, false
@@ -47,19 +112,27 @@ func main() {
 	}
 
 	if found && !behind {
+		var w *git.Worktree
 		fmt.Println("updating repository")
-		w, err := local.Worktree()
+		w, err = local.Worktree()
 
 		if err != nil {
-			fmt.Println(err)
+			return
 		}
 
-		err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+		pullAuth := &git.PullOptions{RemoteName: remoteName, Force: force}
+		if sshAuth != nil {
+			pullAuth.Auth = sshAuth
+		}
+
+		err = w.Pull(pullAuth)
 
 		if err != nil {
-			fmt.Println(err)
+			return
 		}
+		fmt.Println("repository succesfully updated")
 		return
 	}
 	fmt.Println("repository already at latest change")
+	return
 }
