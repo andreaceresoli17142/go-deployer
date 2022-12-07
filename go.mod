f11a1ba20a123181dacdccb15e7814e005018834
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2022-12-07 22:07:16 +0100

    automated commit 07/12/2022 22:07

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
-module test
+module main
 
 go 1.19
 
-require (
-	github.com/go-git/go-git v4.7.0+incompatible
-	github.com/go-git/go-git/v5 v5.5.0
-)
+require github.com/go-git/go-git/v5 v5.5.0
 
 require (
 	github.com/Microsoft/go-winio v0.5.2 // indirect
@@ -18,15 +15,12 @@ require (
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pjbgf/sha1cd v0.2.0 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/skeema/knownhosts v1.1.0 // indirect
-	github.com/src-d/gcfg v1.4.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.2 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
-	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
@@ -26,7 +28,8 @@ type Repository struct {
 	Url     string  `json:"url"`
 	Remote  string  `json:"remote"`
 	Path    string  `json:"path"`
-	Polling int     `json:"polling"`
+	Polling int    `json:"polling"`
+	Force   bool    `json:"force"`
 }
 
 func notify(s string) {
@@ -35,37 +38,71 @@ func notify(s string) {
 
 func main() {
 
-	path := os.Args[1]
-	remoteName := os.Args[2]
+	/*path := os.Args[1]
+	remoteName := os.Args[2]*/
 
+	//TODO: fix the use of ssh keys
+	/*
 	var publicKeys *ssh.PublicKeys
+		if len(os.Args) >= 4 {
+			privateKeyFile := os.Args[3]
+			var password string
 
-	if len(os.Args) >= 4 {
-		privateKeyFile := os.Args[3]
-		var password string
+			if len(os.Args) == 5 {
+				password = os.Args[4]
+			}
 
-		if len(os.Args) == 5 {
-			password = os.Args[4]
+			_, err := os.Stat(privateKeyFile)
+			if err != nil {
+				fmt.Println("read file %s failed %s\n", privateKeyFile, err.Error())
+				return
+			}
+
+			// Clone the given repository to the given directory
+			publicKeys, err = ssh.NewPublicKeysFromFile("git", privateKeyFile, password)
+			if err != nil {
+				fmt.Println("generate publickeys failed: %s\n", err.Error())
+				return
+			}
+			fmt.Println(publicKeys)
 		}
+	*/
 
-		_, err := os.Stat(privateKeyFile)
-		if err != nil {
-			fmt.Println("read file %s failed %s\n", privateKeyFile, err.Error())
-			return
-		}
+	var repositories []Repository
 
-		// Clone the given repository to the given directory
-		publicKeys, err = ssh.NewPublicKeysFromFile("git", privateKeyFile, password)
-		if err != nil {
-			fmt.Println("generate publickeys failed: %s\n", err.Error())
-			return
-		}
-		fmt.Println(publicKeys)
+	if err := loadJson( "repos.json", &repositories ); err != nil {
+		fmt.Println(err)
 	}
 
-	if err := updateIfChanged(publicKeys, path, remoteName, true); err != nil {
+	//fmt.Println(repositories)
+
+	for _, v := range repositories {
+		go startPolling(v)
+	}
+
+	for {}
+
+	/*if err := updateIfChanged(publicKeys, path, remoteName, false); err != nil {
 		fmt.Println(err)
 		notify(err.Error())
+	}*/
+}
+
+func startPolling( repo Repository ) {
+
+	if repo.Polling == 0 {
+		repo.Polling = defaultPolling
+	}
+
+	for {
+		time.Sleep(time.Second * time.Duration(repo.Polling))
+		switch repo.Job {
+		case keepUpdated:
+			err := updateIfChanged(nil, repo.Path, repo.Remote, repo.Force )
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
 	}
 }
 
@@ -75,6 +112,28 @@ func updateIfChanged(sshAuth *ssh.PublicKeys, path string, remoteName string, fo
 
 	if err != nil {
 		return
+	}
+
+	if !force {
+		var w *git.Worktree
+		var s git.Status
+
+		w, err = local.Worktree()
+
+		if err != nil {
+			return
+		}
+
+		s, err = w.Status()
+
+		if err != nil {
+			return
+		}
+
+		if !s.IsClean() {
+			fmt.Println("repository has local change")
+			return
+		}
 	}
 
 	localHead, err := local.Head()
@@ -135,4 +194,18 @@ func updateIfChanged(sshAuth *ssh.PublicKeys, path string, remoteName string, fo
 	}
 	fmt.Println("repository already at latest change")
 	return
+}
+
+func loadJson[T any](FileName string, inp T) error {
+	content, err := os.ReadFile(FileName)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(content, &inp)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
