ff5ca22c10074257733fed3eafb70b2993113d96
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2023-01-11 10:08:45 +0100

    go-deployer auto-commit: 11/1/2023 10:8

--- go-deployer.go
+++ go-deployer.go
@@ -1,5 +1,5 @@
 package main
-
+a
 import (
 	"encoding/json"
 	"fmt"
--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go mod init main || continue
-go mod tidy || continue
-notify-send $(go build . || notify-send("error compiling go-deployer, aborting deployment"); exit 1 ) 
+go mod init go-deployer 2> /dev/null || continue
+go mod tidy 2> /dev/null || continue
+notify-send $(go build . || notify-send "error compiling go-deployer, aborting deployment"; exit 1 ) 
 
-# killall -ew ./go-deployer || continue
-# ./go-deployer &
+killall -ew ./go-deployer || continue
+./go-deployer &
