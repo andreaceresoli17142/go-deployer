4602106c6f98a3fb02238a451f27f53abbf1fc88
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2023-01-11 10:12:13 +0100

    go-deployer auto-commit: 11/1/2023 10:12

--- redeploy.sh
+++ redeploy.sh
@@ -2,7 +2,7 @@
 
 go mod init go-deployer 2> /dev/null || continue
 go mod tidy 2> /dev/null || continue
-notify-send $(go build . || notify-send "error compiling go-deployer, aborting deployment"; exit 1 ) 
+go build . || exit 1 
 
 killall -ew ./go-deployer || continue
 ./go-deployer &
