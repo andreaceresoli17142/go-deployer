6fce23c0a07bddf84fdf2f5431fe2752bd807a5b
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2023-02-18 16:01:00 +0100

    automated commit 18/02/2023 16:00

--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go mod init go-deployer 2> /dev/null || continue
-go mod tidy 2> /dev/null || continue
+go mod init go-deployer 2> /dev/null || :
+go mod tidy 2> /dev/null || :
 go build . 
 
-killall -ew ./go-deployer || continue
+killall -ew ./go-deployer || : 
 ./go-deployer &
